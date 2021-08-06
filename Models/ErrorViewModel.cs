c037b064de840c8047b46d1d0f2895dcbb7a419b
Pham Duy Han <han.phamduy@vmedgroup.io>
2021-08-06 13:43:17 +0700

    Message

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
